f61725494d1a8e52712d7408fac008b64012943f
Karen Holguin <karenhol137@gmail.com>
2024-09-24 19:42:20 -0500

    CHG Designed and structured login page for the case management platform.

--- VBG_TDEA_FRONT/src/index.jsx
+++ VBG_TDEA_FRONT/src/index.jsx
@@ -3,7 +3,6 @@ import React from 'react';;
 import ReactDOM from 'react-dom/client';
 import App from './app';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'
 
 
 const rootElement = document.getElementById('root');
