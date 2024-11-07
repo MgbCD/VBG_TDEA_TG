8d2003107a2d292cd06d27f21cf77aada5d05c5a
Karen Holguin <karenhol137@gmail.com>
2024-11-07 15:52:16 -0500

    ADD installation manual

--- VBG_TDEA_FRONT/src/services/authConfig.jsx
+++ VBG_TDEA_FRONT/src/services/authConfig.jsx
@@ -2,8 +2,8 @@ import { PublicClientApplication } from "@azure/msal-browser";
 
 const msalConfig = {
     auth: {
-        clientId: "23b67e89-4e98-4c62-ba48-e520642a5423",
-        authority: "https://login.microsoftonline.com/2618ef2a-7956-4b9a-b5c7-95fe306a3e7b",
+        clientId: process.env.REACT_APP_AZURE_CLIENT_ID,
+        authority: process.env.REACT_APP_AZURE_AUTHORITY,
         redirectUri: process.env.REACT_APP_REDIRECT_URI,
         navigateToLoginRequestUrl: false,
     },
