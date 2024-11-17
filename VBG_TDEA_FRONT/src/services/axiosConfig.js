fb7719c1eb04d91e57c5206f82c576f0da0a4355
Karen Holguin <karenhol137@gmail.com>
2024-11-16 19:32:07 -0500

    CHG final adjustments

--- VBG_TDEA_FRONT/src/services/axiosConfig.js
+++ VBG_TDEA_FRONT/src/services/axiosConfig.js
@@ -1,25 +1,36 @@
 import axios from 'axios';
-import { useMsal } from '@azure/msal-react';
+import msalInstance from './authConfig';  // Importa la instancia de msal
 import { useNavigate } from 'react-router-dom';
 import Config from '../utils/Config';
 
 const useAxios = () => {
-  const { instance, accounts } = useMsal();
   const navigate = useNavigate();
-
   const baseURL = Config.REACT_APP_API_BASE_URL;
+
   const axiosInstance = axios.create({
     baseURL: baseURL,
   });
 
   axiosInstance.interceptors.request.use(async (config) => {
+    // Usamos la instancia de msal ya inicializada
+    const accounts = msalInstance.getAllAccounts();  // Obtener las cuentas desde msalInstance
     if (accounts.length > 0) {
       const account = accounts[0];
       try {
-        const tokenResponse = await instance.acquireTokenSilent({
-          scopes: ["openid", "profile"],
-          account: account
-        });
+        let tokenResponse;
+        try {
+          // Intentamos obtener el token de forma silenciosa
+          tokenResponse = await msalInstance.acquireTokenSilent({
+            scopes: ["openid", "profile"],  // Ajusta los scopes según sea necesario
+            account: account,
+          });
+        } catch (error) {
+          // Si acquireTokenSilent falla, intenta obtener el token con un popup
+          tokenResponse = await msalInstance.acquireTokenPopup({
+            scopes: ["openid", "profile"],  // Ajusta los scopes según sea necesario
+          });
+        }
+
         const idToken = tokenResponse.idToken;
         config.headers.Authorization = `Bearer ${idToken}`;
       } catch (error) {
@@ -31,15 +42,14 @@ const useAxios = () => {
     return Promise.reject(error);
   });
 
-
   axiosInstance.interceptors.response.use(
     (response) => {
       return response;
     },
     (error) => {
       if (error.response && (error.response.status === 401 || error.response.status === 403)) {
-        navigate('/login');
         localStorage.removeItem('token');
+        navigate('/login');
       }
       return Promise.reject(error);
     }
--- VBG_TDEA_FRONT/src/utils/Config.js
+++ VBG_TDEA_FRONT/src/utils/Config.js
@@ -1,7 +1,7 @@
 const Config = {
     PORT: '3001',
-    REACT_APP_AZURE_CLIENT_ID: '<Your Azure Client ID>',
-    REACT_APP_AZURE_AUTHORITY: 'https://login.microsoftonline.com/<YourTenantID>',
+    REACT_APP_AZURE_CLIENT_ID: '23b67e89-4e98-4c62-ba48-e520642a5423',
+    REACT_APP_AZURE_AUTHORITY: 'https://login.microsoftonline.com/2618ef2a-7956-4b9a-b5c7-95fe306a3e7b',
     REACT_APP_API_BASE_URL: 'http://localhost:3000',
     REACT_APP_REDIRECT_URI: 'http://localhost:3001'
 };
